379346f12879e76d85a45e51ea7ea8a17549a139
Sandeep kumar <sandeepmaurya4512@gmail.com>
2023-09-10 11:39:01 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,61 @@
+let timer = 60;
+let ranNu;
+let Score = 0;
+const whiteConteiner = document.querySelector("#Pbtm");
+
+
+const Hitbuble = () => {
+  ranNu = Math.floor(Math.random() * 10);
+  document.querySelector("#HitValue").textContent = ranNu;
+}
+
+const makeBoble = () => {
+  let Buble = "";
+  for (i = 1; i <= 189; i++) {
+    let ranNumer = Math.floor(Math.random() * 10);
+    Buble += `  <div class="bubble">${ranNumer}</div>`
+  }
+
+  document.querySelector("#Pbtm").innerHTML = Buble;
+}
+
+const RunTimer = () => {
+  setInterval(() => {
+    if (timer > 0) {
+      timer--;
+      document.querySelector("#setTimer").textContent = timer;
+    }
+    else {
+      clearInterval(timer);
+      whiteConteiner.innerHTML = ` <div>
+      <h2>Game Over</h2>
+      <p style="color:rgb(220,0,0);font-size:20px">Score: ${Score}</p>
+      </div>  `;
+
+    }
+
+  }, 1000)
+}
+
+const increseScore = () => {
+  Score += 10;
+  document.querySelector("#scoreValue").textContent = Score;
+}
+
+whiteConteiner.addEventListener('click', (details) => {
+  let clickedNo = details.target.textContent;
+
+  if (clickedNo == ranNu) {
+    increseScore();
+    Hitbuble();
+    makeBoble();
+
+  }
+
+})
+
+
+Hitbuble();
+RunTimer();
+makeBoble();
+
